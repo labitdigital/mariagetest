027161b180c19a43ae64fd7be7fef01af3be54b8
itdigital <contact@youritdigital.com>
2019-01-12 01:14:29 +0100

    first send

---
+++ webpage/index.php
@@ -0,0 +1,453 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Matthew & Mallory</title>
+
+<!-- Bootstrap CSS -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/bootstrap-theme.css" rel="stylesheet">
+<!-- Font Awesome CSS -->
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<!-- Flaticon CSS -->
+<link href="css/flaticon/flaticon.css" rel="stylesheet">
+<!-- Owl Carousel CSS -->
+<link href="css/owl.carousel.css" rel="stylesheet">
+<link href="css/owl.transitions.css" rel="stylesheet">
+<!-- ColorBox CSS -->
+<link href="css/colorbox.css" rel="stylesheet">
+<!-- Theme CSS -->
+<link href="css/style.css" rel="stylesheet">
+<!-- Color CSS -->
+<link href="css/brown.css" rel="stylesheet">
+
+
+<!-- Google Fonts -->
+<link href='http://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700' rel='stylesheet' type='text/css'>
+
+
+</head>
+
+<body>
+
+
+ 	<!-- Header -->
+    <header>
+        <div class="header_plane anim"></div>
+        
+        <!-- Menu Button -->
+        <a class="main_menu_btn">
+            <span class="line line1"></span>
+            <span class="line line2"></span>
+            <span class="line line3"></span>
+        </a>
+        
+        
+        <!-- Submenu -->
+        <div class="main_menu_block">
+            <div class="menu_wrapper">
+                <div class="sub_menu anim">
+                    <ul>
+                        <li><a href="#our_story">Notre histoire</a></li>
+                        <li><a href="#guests">Garçon et filles d'honneurs</a></li>
+                        <li><a href="#when_where">Ou et quand</a></li>
+                        <li><a href="#rsvp">Messages</a></li> 
+                        <li><a href="#gallery">Quelques Photos</a></li> 
+                                          
+                    </ul>
+                </div>
+                <div class="sub_img anim"></div>
+            </div>
+        </div>
+        <!-- Submenu End -->
+        
+		<!-- Social Buttons -->
+        <div class="header_social">
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-pinterest"></i></a>
+        </div>	
+        
+    </header>
+    <!-- Header End -->
+
+<div class="page">
+ 
+
+    <!-- Intro -->
+    <section class="home_intro white_txt intro_slider">        
+
+        <!-- Slider -->
+        <div class="slider">
+            <div class="slider_item parallax" data-image="images/slider/1.jpg"></div>
+            <div class="slider_item parallax" data-image="images/slider/young-black-couple.jpg"></div>
+            
+        </div>
+
+        <div class="home_txt" data-0="opacity:1" data-top-bottom="opacity:0">
+            <!-- Intro Text -->
+            
+            <div class="title1 title1_2">Gertrude & Franck</div>
+            <div class="title1 title1_2">se marient</div>
+            <div class="bullet"><span>23 <b>12</b></span></div>
+            <div class="title0">2018</div>
+            <a href="#married" class="intro_down"><span><i class="fa fa-angle-down"></i></span></a>
+        </div>
+        <div class="into_firefly"></div>   
+    </section>
+    <!-- Intro End -->
+    
+    <!-- Married -->
+    <section class="married clearfix" id="married">
+        <div class="container">
+                <div class="im1 parallax"  data-bottom="left:20%" data-center="left:0" data-image="images/portrait/franck.png"><div class="im_arrows"></div></div>
+                <div class="im2 parallax" data-bottom="right:20%" data-center="right:0" data-image="images/portrait/gertrude.png"><div class="im_arrows"></div></div>
+
+                <div class="married_txt" data-bottom="opacity:0" data-center="opacity:1">
+                    <h2><span>le jour</span>  J</h2>
+                    Le 23 decembre , 2018<br>
+                    <b>Gertrude Moukouri & Franck Ngouchet</b><br>
+                    CASTEL HALL Bonapriso<br>
+                     
+                    
+                    <div class="married_coundown"></div>
+                    <div class="double_arrow"></div>
+                </div>
+        </div>
+    </section>
+    <!-- Married End -->
+
+    
+    <!-- Story -->
+    <section class="our_story" id="our_story">
+            <h2><span>Notre</span>  Histoire</h2>
+            
+            <!-- Wrapper -->
+            <div class="story_wrapper">
+                
+                <!-- Item -->
+                <div class="story_item">
+                    <div class="story_img parallax" data-image="images/histoire/rencontre.jpg"><div class="story_img_plane"></div></div>
+                    <div class="story_plane">
+                        <div class="story_back"></div>
+                        <div class="story_txt">
+                            <div class="story_title">
+                                Notre rencontre 
+                                <i>03.12.2013</i>
+                            </div>
+                             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                             quis nostrud exercitation ullamco.
+                        </div> 
+                    </div>
+                </div>
+
+                <!-- Item -->
+                <div class="story_item">
+                    <div class="story_img parallax" data-image="images/histoire/premier-voyage.png">
+                       
+                    <div class="story_img_plane"></div></div>
+                    <div class="story_plane">
+                        <div class="story_back"></div>
+                        <div class="story_txt">
+                              <div class="story_title">
+                                Notre premier voyage
+                                <i>03.01.2014</i>
+                            </div>
+                             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                             quis nostrud exercitation ullamco.
+                        </div> 
+                    </div>
+                </div>
+
+                <!-- Item -->
+                <div class="story_item">
+                    <div class="story_img parallax" data-image="images/histoire/bebe.jpg"><div class="story_img_plane"></div></div>
+                    <div class="story_plane">
+                        <div class="story_back"></div>
+                        <div class="story_txt">
+                              <div class="story_title">
+                                Notre bébé d'amour
+                                <i>12.05.2018</i>
+                            </div>
+                             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                             quis nostrud exercitation ullamco.
+                        </div> 
+                    </div>
+                </div>
+
+                <!-- Item -->
+                <div class="story_item">
+                    <div class="story_img parallax" data-image="images/histoire/yes.jpg"><div class="story_img_plane"></div></div>
+                    <div class="story_plane">
+                        <div class="story_back"></div>
+                        <div class="story_txt">
+                              <div class="story_title">
+                                La demande
+                                <i>24.08.2014</i>
+                            </div>
+                             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                             quis nostrud exercitation ullamco.
+                        </div> 
+                    </div>
+                </div>
+
+                
+                <!-- Item -->
+                <div class="story_item">
+                    <div class="story_img parallax" data-image="images/histoire/attente.png"><div class="story_img_plane"></div></div>
+                    <div class="story_plane">
+                        <div class="story_back"></div>
+                        <div class="story_txt">
+                              <div class="story_title">
+                                Nous vous attendons
+                                <i>03.02.2018</i>
+                            </div>
+                             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                             quis nostrud exercitation ullamco.
+                        </div> 
+                    </div>
+                </div>
+                 
+        </div>
+    </section>
+
+    <!-- The Crew -->
+    <section class="guests" id="guests">
+        <div class="container">
+            <h2><span>Les</span> Hôtes</h2>
+            
+             <!-- Guest Wrapper -->
+            <div class="guest_wrapper" >
+            <!-- Best Man & Maid of Honor  -->
+            
+            
+            <div class="guest_item">
+                <div class="guest_photo"><img src="images/honneur/homme3.jpg" alt="Best Man"></div>
+                <div class="guest_title"><span>Garçon d'honneur</span> Maxx Fokou </div>
+                <i class="flaticon-balloon14"></i> Trop swag
+                
+            </div>
+            <div class="guest_item">
+                <div class="guest_photo"><img src="images/honneur/femme2.jpg" alt="Maid Of Honor"></div>
+                <div class="guest_title"><span>Fille d'honneur</span> Alina Takam </div>
+                 <i class="flaticon-balloon14"></i> Gros bisous
+
+            </div>
+            
+            <div class="guest_item">
+                <div class="guest_photo"><img src="images/honneur/homme4.jpg" alt="James"></div>
+                <div class="guest_title"><span>Garçon d'honneur</span> Jean Fingon</div>
+                <i class="flaticon-balloon14"></i>  Que du bonheur
+
+            </div>
+            <div class="guest_item">
+                <div class="guest_photo"><img src="images/honneur/femme3.jpg" alt="Justin"></div>
+                <div class="guest_title"><span>Fille d'honneur</span> Justine Yakana </div>
+                <i class="flaticon-balloon14"></i>ça va chauffer
+
+            </div>
+            
+            
+            </div>
+            <!-- Guest Wrapper End -->
+        </div>   
+    </section>
+    <!-- The Crew End -->
+
+    
+
+    <!-- When & Where -->
+    <section class="when_where white_txt parallax" id="when_where" data-image="images/mari-ecran.jpg" data-bottom-top="opacity:0;" data-bottom="opacity:1;">
+        <div class="over"></div>
+        <div class="container">
+               
+            <!-- Photocamera Icon -->
+            <div class="photocamera"><span class="flaticon-slr2"></span></div>
+
+            <div class="when_where_container opacity">
+            <h2><span>Ou &amp;</span> Quand ?</h2>
+                
+                <!-- Texts -->
+                <div class="pattern1"></div>
+                <div class="title1">Le CASTEL HALL</div>
+                <div class="title2">Bonapriso</div>
+                <div class="title3"><a href="https://goo.gl/maps/dhx42" target="_blank">
+                Mobil NJO NJO </a><br /><i class="flaticon-map35"></i> </div>
+
+                <div class="pattern2"></div>
+                <div class="title1">19H</div>
+                <div class="title4">23 Decembre </div>
+                <div class="pattern3"></div>
+            </div>
+        </div>
+    </section>
+    <!-- When & Where End -->
+  
+    
+      
+    <!-- RSVP -->
+    <section class="rsvp" id="rsvp">
+        <div class="container">
+            <h2><span>Laisse nous</span> un mot gentil</h2>
+
+            <div id="envelope" data-100-top="@class:active" data-200-bottom="@class: ">
+                <div class="envelope_front"><div class="env_top_top"></div></div>
+                <div class="envelope_back"><div class="env_top"></div></div>
+
+                <div class="paper">
+       
+                    <!-- End Date of Reservation -->
+                   
+                    
+                    <!-- Form -->
+                    <div id="div_block_1">
+                        <div class="txt_input">
+                            <input type="text" class="form-control" id="name_block_1"  placeholder="Ton nom">
+                        </div>
+                        <div class="txt_input">
+                            <input type="text" class="form-control" id="email_block_1"  placeholder="Ton e-mail">
+                        </div>
+                        <div class="textarea">
+                            <input type="textarea" class="form-control" id="attending_block_1"  placeholder="message">
+                        </div>
+                        <input name="" type="submit" value="Send" class="btn btn-lg submit_block_1">
+                       
+                       
+                        <!-- Form Additional text -->
+                        <p>Nous sommes impatient de vos y voir. A bientot!</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section> 
+    <!-- RSVP End -->
+
+
+    <!-- Gallery -->
+    <section class="gallery" id="gallery">
+        <h2><span>Quelques</span> photos</h2>    
+        <div class="gallery_wrapper">
+            <div class="gallery_item">
+                <div class="gallery_txt">
+                   <a href="images/gallery/1.png" title="In late August, on a family vacation to Oahu"><b>Hawaii</b>
+                   In late August, on a family vacation to Oahu
+                   </a>
+                </div>
+                <img src="images/gallery/1.png" alt="Gallery 1">    
+            </div>
+            <div class="gallery_item">
+                <div class="gallery_txt">
+                    <a href="images/gallery/2.png" title="In Summer 2015"><b>New Zealand</b>
+                    In Summer 2015
+                    </a>
+                </div>
+                <img src="images/gallery/2.png" alt="Gallery 2">    
+            </div>
+            <div class="gallery_item">
+                <div class="gallery_txt">
+                    <a href="http://placehold.it/640x400" title="February 5, 2015 / New-York, USA"><b>Honeymoon</b>
+                    </a>
+                </div>
+                <img src="http://placehold.it/640x400" alt="Gallery 3">    
+            </div>
+            <div class="gallery_item">
+                <div class="gallery_txt">
+                    <a href="http://placehold.it/640x400" title="February 5, 2015 / New-York, USA"><b>Honeymoon</b>
+                    February 5, 2015 / New-York, USA
+                    </a>
+                </div>
+                <img src="http://placehold.it/640x400" alt="Gallery 4">    
+            </div>
+            <div class="gallery_item">
+                <div class="gallery_txt">
+                   <a href="http://placehold.it/640x400" title="In late August, on a family vacation to Oahu"><b>Hawaii</b>
+                   In late August, on a family vacation to Oahu
+                   </a>
+                </div>
+                <img src="http://placehold.it/640x400" alt="Gallery 5">    
+            </div>
+            <div class="gallery_item">
+                <div class="gallery_txt">
+                    <a href="http://placehold.it/640x400" title="In Summer 2015"><b>New Zealand</b>
+                    In Summer 2015<br>
+                    </a>
+                </div>
+                <img src="http://placehold.it/640x400" alt="Gallery 6">    
+            </div>
+        </div>
+    </section>
+    <!-- Gallery End -->
+
+    
+
+    <!-- Footer -->
+    <section class="footer white_txt parallax" id="footer" data-image="images/slider/young-black-couple.jpg">
+        <div class="over"></div>
+        <div class="container">
+
+             <!-- Thanks Text -->
+            <div class="thanks">Merci de partager notre bonheur</div>
+            <div class="footer_txt">
+
+               
+
+
+                <!-- Social Buttons -->
+                <div class="footer_social">
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-pinterest"></i></a>
+                </div>
+                
+                            <div class="title1">Gertrude & franck</div>
+                <!-- Copyrights -->
+                <div class="copyrights">2018 Gertrude & Franck - powered by IT & DIGITAL</div>
+                
+            </div>
+        </div>
+
+         
+
+    </section> 
+
+    
+    <!-- JQuery -->
+	<script src="js/jquery-1.11.0.min.js"></script> 
+    <!-- CountDown JS -->
+    <script type="text/javascript" src="js/jquery.plugin.min.js"></script> 
+	<script type="text/javascript" src="js/jquery.countdown.min.js"></script>
+    <script type="text/javascript" src="js/jquery.onepage-scroll.min.js"></script>
+
+ 	<!-- ColorBox JS -->
+    <script src="js/jquery.colorbox-min.js"></script>
+    <!-- OWL Carousel JS -->
+	<script src="js/owl.carousel.min.js"></script>
+    <!-- Bootstrap JS -->
+	<script src="js/bootstrap.min.js"></script>
+
+    <!-- ScrollR JS -->
+    <script src="js/skrollr.min.js"></script>
+    
+
+    
+    <!-- PrefixFree -->
+    <script src="js/prefixfree.min.js"></script>
+    
+    <!-- FireFly JS -->
+    <script src="js/jquery.firefly-0.3-min.js"></script>
+    <!-- Theme JS -->
+    <script src="js/script.js"></script>
+    
+ 
+</div>
+
+</body>
+</html>
